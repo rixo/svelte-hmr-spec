f30962c76a26ae98692273a3e2bacee9bca372e0
rixo <rixo@rixo.fr>
2021-01-25 18:31:12 +0100

    add test for uninitialized props

--- test/props.spec.js
+++ test/props.spec.js
@@ -49,6 +49,57 @@ describe('props', () => {
   `
 
   testHmr`
+    # preserves value of uninitialized props
+
+    ${function*() {
+      yield this.cons.ignoreWarnings(
+        "<Child> was created without expected prop 'name'"
+      )
+    }}
+
+    --- App.svelte ---
+
+    <script>
+      import Child from './Child.svelte'
+
+      let name
+
+      // NOTE ensure _changed_ (i.e. current) props are preserved too, not just
+      // initial props -- bug detected on 2021-01-25
+      const changeName = () => {
+        name = 'foo'
+      }
+    </script>
+
+    <x-focus>
+      ::0 <Child />
+      ::1 <Child {name} />
+    </x-focus>
+
+    <button on:click={changeName} />
+
+    --- Child.svelte ---
+
+    <script>
+      export let name
+    </script>
+
+    ::0 I am {name}
+    ::2 My name is {name}
+
+    * * *
+
+    ::0::
+      I am undefined
+    ::1::
+      I am undefined
+      ${clickButton()}
+      I am foo
+    ::2::
+      My name is foo
+  `
+
+  testHmr`
     # preserves props value with preserveLocalState
 
     --- App.svelte ---
